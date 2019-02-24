f3d341a3dfc0fd37836a2a3b019b3109819dadf2
Lauren Days <lmchambers0420@gmail.com>
2019-02-24 03:49:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,17 @@
-class Song 
+class Song
   attr_accessor :name, :artist
-  
+  @@all = []  # Create an empty array to track all song instances
+
   def initialize(name)
-    @name = name 
-  end 
-  
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
+    @name = name
+    @@all << self # Add new song instance to tracker array
   end
-  
+
+  def artist_name
+    # Check to see if the song has an artist variable
+    # If it does, assign the artist variable the value of the artist's name
+    # If it doesn't, return nil
+    self.artist ? self.artist.name : nil
+  end
+
 end 
