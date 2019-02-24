a24518a43c680aa840dc42080ab7109db09d587c
Lauren Days <lmchambers0420@gmail.com>
2019-02-24 03:57:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,26 +1,33 @@
 class Author
   attr_accessor :name
-  attr_reader :posts  # Set posts array to read-only
-  @@post_count = 0  # Set post instance count tracker to zero
+  @@post_count = 0
 
   def initialize(name)
     @name = name
-    @posts = [] # Create an empty array for the author's post instances
+    @posts = []
+
+  end
+
+  def posts
+    @posts
+
   end
 
   def add_post(post)
-    self.posts << post  # Add the post instance to the author's list of posts
-    post.author = self  # Assign the author to the post instance
-    @@post_count += 1   # Add one post instance to the count tracker
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
-    post = Post.new(title)  # Create a new post instance with a title
-    add_post(post)  # Execute add post method using newly created post instance
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
   def self.post_count
-    @@post_count  # Return the total count of post instances
+    @@post_count
   end
-
+  
 end
