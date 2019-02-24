250691fb40ddd4c473ff14e4d72d27bc54b1162a
Lauren Days <lmchambers0420@gmail.com>
2019-02-24 04:04:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,6 @@ class Song
   
   @@all = [ ]
     
-  #The Song.new method listed below is initialized with an argument of a name
-  #and pushes new instances into a class variable called @@all upon initialization. 
-  #Note that the method is really listed as initialize instead of .new
   def initialize(name)
       @name = name
       @@all << self
@@ -15,8 +12,6 @@ class Song
     @@all
   end
 
-  #The artist_name method allows the song to know the name of its artist
-  #but returns nil if the song does not have an artist
   def artist_name
     if self.artist != nil
         self.artist.name
