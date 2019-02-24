f999df10ab455414f0db948f5d0c36ac1e8887cc
Lauren Days <lmchambers0420@gmail.com>
2019-02-24 04:04:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,28 @@
 class Song
   attr_accessor :name, :artist
-  @@all = []  # Create an empty array to track all song instances
-
+  
+  @@all = [ ]
+    
+  #The Song.new method listed below is initialized with an argument of a name
+  #and pushes new instances into a class variable called @@all upon initialization. 
+  #Note that the method is really listed as initialize instead of .new
   def initialize(name)
-    @name = name
-    @@all << self # Add new song instance to tracker array
+      @name = name
+      @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  #The artist_name method allows the song to know the name of its artist
+  #but returns nil if the song does not have an artist
   def artist_name
-    # Check to see if the song has an artist variable
-    # If it does, assign the artist variable the value of the artist's name
-    # If it doesn't, return nil
-    self.artist ? self.artist.name : nil
+    if self.artist != nil
+        self.artist.name
+    else
+        nil
+    end
   end
-
-end 
+  
+end
