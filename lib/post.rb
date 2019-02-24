580920eec9996d517f964af562a4ad8bdea17229
Lauren Days <lmchambers0420@gmail.com>
2019-02-24 03:53:24 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,17 +1,27 @@
 class Post
-  attr_accessor :title, :author
-  @@all = []  # Create an empty array to track all post instances
-
-  def initialize(title)
-    @title = title
-    @@all << self # Add new post instance to tracker array
+  attr_accessor :name, :author  
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+    @@all << self
   end
-
+  
   def author_name
-    # Check to see if the post has an author variable
-    # If it does, assign the author variable the value of the author's name
-    # If it doesn't, return nil
-    self.author ? self.author.name : nil
+    if self.author
+      self.author.name
+    else
+      nil
+    end
   end
-
+  
+  def title
+    title = self.name
+  end
+  
+  def all
+    @@all
+  end
 end
