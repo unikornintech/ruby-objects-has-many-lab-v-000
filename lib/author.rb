92852909542feb568bcd41bdd6010a460a4eaa33
Lauren Days <lmchambers0420@gmail.com>
2019-02-24 03:35:16 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,33 @@
-class Author 
+class Author
   attr_accessor :name
- 
-  
+  @@post_count = 0
+
   def initialize(name)
-    @name = name 
+    @name = name
     @posts = []
-  end 
-  
+
+  end
+
   def posts
     @posts
-  end 
-  
+
+  end
+
   def add_post(post)
-    new_post = Post.new(post)
-    post.author = self 
-    @posts << post 
-  end 
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
   
-end 
+end
