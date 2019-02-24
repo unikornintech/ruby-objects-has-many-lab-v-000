34fa77c8aef517040d9af16853e190bf8f915a21
Lauren Days <lmchambers0420@gmail.com>
2019-02-24 03:36:07 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,18 @@
 class Post 
   
-  attr_accessor :author 
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
   
 end 
